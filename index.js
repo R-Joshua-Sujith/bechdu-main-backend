9da97a9aff16903d1ee7521531fbde71fb73b1c8
R-Joshua-Sujith <joshuasujith14@gmail.com>
2024-02-02 22:12:10 +0530

    completed

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors")
 const dotenv = require("dotenv");
-const bodyParser = require('body-parser');
-
 
 const categoryRoutes = require('./routes/category');
 
 dotenv.config();
 const app = express();
-app.use(express.json());
 app.use(cors());
 
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DB Connection Successful"))
