6b02c46bed6885c3dfc9de4c13640644ed16d3ad
R-Joshua-Sujith <joshuasujith14@gmail.com>
2024-02-21 16:28:51 +0530

    completed

--- routes/promoCode.js
+++ routes/promoCode.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const PromoCodeModel = require("../models/PromoCode")
-
+const UserModel = require("../models/User")
 
 router.post('/create/promocode', async (req, res) => {
     try {
@@ -19,10 +19,18 @@ router.post('/create/promocode', async (req, res) => {
 
 router.get('/get-all-promocode', async (req, res) => {
     try {
-        const { page = 1, pageSize = 5 } = req.query;
+        const { page = 1, pageSize = 5, search = '' } = req.query;
         const skip = (page - 1) * pageSize;
 
-        const allPromoCode = await PromoCodeModel.find().skip(skip).limit(parseInt(pageSize));
+        const searchRegex = new RegExp(search, 'i');
+
+        const query = {
+            $or: [
+                { code: searchRegex },
+            ],
+        }
+
+        const allPromoCode = await PromoCodeModel.find(query).skip(skip).limit(parseInt(pageSize));
         const totalPromoCode = await PromoCodeModel.countDocuments();
 
         res.json({
@@ -91,4 +99,30 @@ router.delete('/delete/promocode/:id', async (req, res) => {
     }
 });
 
+
+router.post('/check/promocode', async (req, res) => {
+    try {
+        const { enteredCode, phone } = req.body;
+
+        // Find the promo code in the database
+        const promoCode = await PromoCodeModel.findOne({ code: enteredCode });
+
+        if (!promoCode) {
+            return res.json({ valid: false, message: 'Invalid promo code' });
+        }
+
+        // Check if the promo code is already used by the user
+        const user = await UserModel.findOne({ phone: phone, promoCodes: enteredCode });
+
+        if (user) {
+            return res.json({ valid: false, message: 'Promo code already used by this user' });
+        }
+
+        // If the promo code is valid and not used by the user, return its value
+        res.json({ valid: true, value: promoCode.discountAmount, message: 'Promo Code Applied Successfully' });
+    } catch (error) {
+        console.error('Error checking promo code validity:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 module.exports = router;
